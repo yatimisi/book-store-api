0bf6c8aa56992b1f113e7f39b3155dc391c4607d
Yatimisi <yatimisi2012@gmail.com>
2019-01-30 10:41:31 +0800

    update

---
+++ books/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Book(models.Model):
+    name = models.CharField('書名', max_length=50)
+    price = models.PositiveIntegerField('價格')
+
+    def __str__(self):
+        return self.name
